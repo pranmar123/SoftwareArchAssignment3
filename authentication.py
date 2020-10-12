cd381f81d0f16c3b752215c6d700062fcc66a8a8
pranmar123 <pranmar123@gmail.com>
2020-10-12 05:43:04 -0500

    Finish authentication

--- authentication.py
+++ authentication.py
@@ -1,2 +1,17 @@
+def enterLoginInfo():
+    username = input("Please enter your username: ")
+    password = input("Please enter your password: ")
+    return username,password
+
 def checkLoginInfo(username, password):
-    print("pass")
+    sucess = False
+    #concatenate user and password to textfile format
+    loginDetails = username + '@' + password
+    with open("credentials.txt","r") as credentials:
+        for line in credentials:
+            #stripping the \n
+            line = line.rstrip("\n")
+            if loginDetails == line:
+                sucess = True
+                break
+    return sucess
--- main.py
+++ main.py
@@ -2,5 +2,12 @@ import authentication
 
 def main():
     print("Welcome to the Online Shopping Center (OSC)")
-    username = input("Please enter your username: ")
-    password = input("Please enter your password: ")
+    username, password = authentication.enterLoginInfo()
+    #create a sucess boolean that returns true if authentication is sucessful
+    sucess = authentication.checkLoginInfo(username,password)
+    #if sucess is false then exit the program
+    if sucess == False:
+        raise Exception("The credentials entered are not valid. Exiting the program..")
+    print("Login credentials accepted.")
+    ##FINISHED the first part bullet of the requirements.
+main()
