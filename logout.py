68a7354814346cbf972abc68e2cee2879cc1fbaf
JoeFlynt <51708024+JoeFlynt@users.noreply.github.com>
2020-10-13 17:07:23 -0500

    Add files via upload
    added logout feature

---
+++ logout.py
@@ -0,0 +1,5 @@
+def logoutUser(userInput):
+    if userInput=="q":
+        pass
+        print("You are now being logged out. ")
+    return 1
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import authentication
-
+import logout
 def main():
+    logoutNow=0
     print("Welcome to the Online Shopping Center (OSC)")
     username, password = authentication.enterLoginInfo()
     #create a sucess boolean that returns true if authentication is sucessful
@@ -10,4 +11,9 @@ def main():
         raise Exception("The credentials entered are not valid. Exiting the program..")
     print("Login credentials accepted.")
     ##FINISHED the first part bullet of the requirements.
+    while logoutNow==0:
+        pass
+        userChoice=input("Please choose an option: ")
+        logoutNow=logout.logoutUser(userChoice)
+        
 main()
