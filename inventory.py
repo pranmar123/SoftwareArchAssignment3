49c33442efb7a74edbfecb4ad40c70e660a0c623
Joe Flynt <jbf240@msstate.edu>
2020-10-14 10:15:30 -0500

    added a count to inventory class to show number of inventory  items

--- inventory.py
+++ inventory.py
@@ -1,31 +1,32 @@
 #Inventory
 
 class inventory:
-    def __init__(self, category, item, price):        
+    def __init__(self, category, item, price, count):        
         self.category = category    
         self.item = item
         self.price = price
+        self.count = count
 
 
-i1 = inventory("Household Goods", "Lamp", float(30.99))
-i2 = inventory("Household Goods", "Broom", float(8.99))
-i3 = inventory("Household Goods", "Chair", float(45.99))
-i4 = inventory("Household Goods", "Candle", float(10.99))
+i1 = inventory("Household Goods", "Lamp", float(30.99), int(10))
+i2 = inventory("Household Goods", "Broom", float(8.99), int(20))
+i3 = inventory("Household Goods", "Chair", float(45.99), int(30))
+i4 = inventory("Household Goods", "Candle", float(10.99), int(40))
 
-i5 = inventory("Books", "SW Testing", float(150.99))
-i6 = inventory("Books", "Algebra", float(150.99))
-i7 = inventory("Books", "History", float(150.99))
-i8 = inventory("Books", "Physics", float(150.99))
+i5 = inventory("Books", "SW Testing", float(150.99),int(10))
+i6 = inventory("Books", "Algebra", float(150.99),int(20))
+i7 = inventory("Books", "History", float(150.99), int(30))
+i8 = inventory("Books", "Physics", float(150.99), int(40))
 
-i9 = inventory("Toys", "Iron Man", float(21.99))
-i10 = inventory("Toys", "Thor", float(21.99))
-i11 = inventory("Toys", "Hulk", float(21.99))
-i12 = inventory("Toys", "Spiderman", float(21.99))
+i9 = inventory("Toys", "Iron Man", float(21.99), int(10))
+i10 = inventory("Toys", "Thor", float(21.99), int(20))
+i11 = inventory("Toys", "Hulk", float(21.99), int(30))
+i12 = inventory("Toys", "Spiderman", float(21.99), int(40))
 
-i13 = inventory("Small Electronics", "Headphones", float(50.99))
-i14 = inventory("Small Electronics", "Phone Charger", float(20.99))
-i15 = inventory("Small Electronics", "Game Controller", float(80.99))
-i16 = inventory("Small Electronics", "Speaker", float(40.99))
+i13 = inventory("Small Electronics", "Headphones", float(50.99), int(10))
+i14 = inventory("Small Electronics", "Phone Charger", float(20.99), int(30))
+i15 = inventory("Small Electronics", "Game Controller", float(80.99), int(30))
+i16 = inventory("Small Electronics", "Speaker", float(40.99), int(40))
 
 
 
